f61e8d580dde6644b3a46f809b5d9f7d2cf81456
groth00 <busygroth@gmail.com>
2018-01-24 13:54:56 -0800

    encoding done, pipeline not working

--- kaggle/kaggle.py
+++ kaggle/kaggle.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.pipeline import make_pipeline
+from sklearn.tree import DecisionTreeRegressor
+
 
 ohe = OneHotEncoder()
 label = LabelEncoder()
+sc = StandardScaler()
 
 df = pd.read_csv('~/Desktop/kaggle/train.csv')
 # 81 columns, first one is the sample number
@@ -14,7 +21,7 @@ df = pd.read_csv('~/Desktop/kaggle/train.csv')
 total = df.isnull().sum().sort_values(ascending=False)
 percent = (df.isnull().sum()/df.isnull().count()).sort_values(ascending=False)
 missing_data = pd.concat([total, percent], axis=1, keys=['Total', 'Percent'])
-print(missing_data.head(n=19))
+# print(missing_data.head(n=19))
 
 
 #drop columns with missing values
@@ -26,12 +33,27 @@ missing_value = df_train.loc[df_train['Electrical'].isnull()].index
 df2 = df_train.drop(missing_value)
 
 #check to make sure the columns were dropped
-print(df_train.head(n=3))
-print(df2.head(n=3))
+# print(df_train.head(n=3))
+# print(df2.head(n=3))
 
 
-#want to encode the categorical variables, so we don't care about the numerical ones for now
+#encode the categorical variables and join with numerical variables
 categorical_variables = df2.select_dtypes(exclude=np.number)
+numerical_variables = df2.select_dtypes(include=np.number)
+encoded_variables = pd.get_dummies(categorical_variables)
+
+#check to make sure the dimensions will match
+print(numerical_variables.shape)
+print(encoded_variables.shape)
+
+processed_df = pd.concat([numerical_variables, encoded_variables], axis=1)
+print(processed_df.head(n=5))
+
+
+#split the training data and fit through pipeline
+# train_test_split(processed_df)
+pipe_dt = make_pipeline(StandardScaler(), PCA(n_components=2), DecisionTreeRegressor(random_state=1))
+pipe_dt.fit(processed_df)
 
 
 #encoding one categorical variable
@@ -42,17 +64,11 @@ categorical_variables = df2.select_dtypes(exclude=np.number)
 # print(ohe_labels.toarray())
 
 
-#encode all of the categorical variables
-for i in range(categorical_variables.shape[1]):
-    int_labels = label.fit_transform(categorical_variables.iloc[:, i]).reshape(-1, 1)
-    ohe_labels = ohe.fit_transform(int_labels)
-
-#need to save encoded categorical variables and join with numerical variables
 
 
-
+#TODO:
 #standardize values
 #feature selection/PCA
-#maybe k-cross-fold validation?
+#k-fold cross validation?
 #grid search to tune hyperparameters
 #pipeline to test models
