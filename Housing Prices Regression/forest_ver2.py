06b45dfb7ca46157ffc53ee4a78f4f9290eda4c8
groth00 <busygroth@gmail.com>
2018-02-06 18:29:01 -0800

    update 2/6

--- Housing Prices Regression/forest_ver2.py
+++ Housing Prices Regression/forest_ver2.py
@@ -173,7 +173,7 @@ map_values()
 df.drop("LotAreaCut",axis=1,inplace=True)
 df.drop(['SalePrice'],axis=1,inplace=True)
 
-print(df.shape)
+# print(df.shape)
 
 ###PIPELINE
 
@@ -223,7 +223,7 @@ data_pipe = pipe.fit_transform(df_copy)
 
 
 #deal with outliers
-scaler = RobustScaler()
+scaler = RobustScaler(copy=False)
 
 #split X into training and test samples, as well as setting y to house prices of training set
 n_train=df_train.shape[0]
@@ -236,13 +236,14 @@ X_scaled = scaler.fit(X).transform(X)
 y_log = np.log(df_train.SalePrice)
 test_X_scaled = scaler.transform(test_X)
 
+
 #visualize the importance of features
-lasso = Lasso(alpha=0.001)
-lasso.fit(X_scaled, y_log)
-Fl_lasso = pd.DataFrame({'Feature Importance':lasso.coef_}, index=data_pipe.columns)
-Fl_lasso.sort_values("Feature Importance",ascending=False)
-Fl_lasso[Fl_lasso["Feature Importance"]!=0].sort_values("Feature Importance").plot(kind="barh",figsize=(15,25))
-plt.xticks(rotation=90)
+# lasso = Lasso(alpha=0.001)
+# lasso.fit(X_scaled, y_log)
+# Fl_lasso = pd.DataFrame({'Feature Importance':lasso.coef_}, index=data_pipe.columns)
+# Fl_lasso.sort_values("Feature Importance",ascending=False)
+# Fl_lasso[Fl_lasso["Feature Importance"]!=0].sort_values("Feature Importance").plot(kind="barh",figsize=(15,25))
+# plt.xticks(rotation=90)
 # plt.show()
 
 
@@ -258,32 +259,135 @@ models = [LinearRegression(), Ridge(), Lasso(alpha=0.01, max_iter=10000), Random
           SGDRegressor(max_iter=1000, tol=1e-3), BayesianRidge(), KernelRidge(alpha=0.6, kernel='polynomial',
           degree=2, coef0=2.5), ExtraTreesRegressor()]
 names = ['LR', 'Ridge', 'Lasso', 'RF', 'GBR', 'SVR', 'LinSVR', 'Ela', 'SGD', 'Bay', 'Ker', 'Extra']
-for name, model in zip(names, models):
-    score = rmse_cv(model, X_scaled, y_log)
-    print('{}: {:.6f}, {:.4f}'.format(name, score.mean(), score.std()))
+# for name, model in zip(names, models):
+#     score = rmse_cv(model, X_scaled, y_log)
+#     print('{}: {:.6f}, {:.4f}'.format(name, score.mean(), score.std()))
 
 
 #define grid search method
-class grid():
-    def __init__(self, model):
-        self.model = model
+# class grid():
+#     def __init__(self, model):
+#         self.model = model
+#
+#     def grid_get(self, X, y, param_grid):
+#         grid_search = GridSearchCV(self.model, param_grid, cv=5, scoring='neg_mean_squared_error',
+#                                    return_train_score=True)
+#         grid_search.fit(X, y)
+#         print(grid_search.best_params_, np.sqrt(-grid_search.best_score_))
+#         grid_search.cv_results_['mean_test_score'] = np.sqrt(-grid_search.cv_results_['mean_test_score'])
+#         print(pd.DataFrame(grid_search.cv_results_)[['params', 'mean_test_score', 'std_test_score']])
 
-    def grid_get(self, X, y, param_grid):
-        grid_search = GridSearchCV(self.model, param_grid, cv=5, scoring='neg_mean_squared_error')
-        grid_search.fit(X, y)
-        print(grid_search.best_params_, np.sqrt(-grid_search.best_score_))
-        grid_search.cv_results_['mean_test_score'] = np.sqrt(-grid_search.cv_results_['mean_test_score'])
-        print(pd.DataFrame(grid_search.cv_results_)[['params', 'mean_test_score', 'std_test_score']])
-
-grid(Lasso()).grid_get(X_scaled,y_log,{'alpha': [0.0004,0.0005,0.0007,0.0006,0.0009,0.0008],'max_iter':[10000]})
-grid(Ridge()).grid_get(X_scaled,y_log,{'alpha':[35,40,45,50,55,60,65,70,80,90]})
-grid(SVR()).grid_get(X_scaled,y_log,{'C':[11,12,13,14,15],'kernel':["rbf"],"gamma":[0.0003,0.0004],
-                                     "epsilon":[0.008,0.009]})
-
-param_grid={'alpha':[0.2,0.3,0.4,0.5], 'kernel':["polynomial"], 'degree':[3],'coef0':[0.8,1,1.2]}
-grid(KernelRidge()).grid_get(X_scaled,y_log,param_grid)
-
-grid(ElasticNet()).grid_get(X_scaled,y_log,{'alpha':[0.0005,0.0008,0.004,0.005],'l1_ratio':[0.08,0.1,0.3,0.5,0.7],
-                                            'max_iter':[10000]})
+# grid(Lasso()).grid_get(X_scaled,y_log,{'alpha': [0.0004,0.0005,0.0007,0.0006,0.0009,0.0008],'max_iter':[10000]})
+# grid(Ridge()).grid_get(X_scaled,y_log,{'alpha':[35,40,45,50,55,60,65,70,80,90]})
+# grid(SVR()).grid_get(X_scaled,y_log,{'C':[11,12,13,14,15],'kernel':["rbf"],"gamma":[0.0003,0.0004],
+#                                      "epsilon":[0.008,0.009]})
+#
+# param_grid={'alpha':[0.2,0.3,0.4,0.5], 'kernel':["polynomial"], 'degree':[3],'coef0':[0.8,1,1.2]}
+# grid(KernelRidge()).grid_get(X_scaled,y_log,param_grid)
+#
+# grid(ElasticNet()).grid_get(X_scaled,y_log,{'alpha':[0.0005,0.0008,0.004,0.005],'l1_ratio':[0.08,0.1,0.3,0.5,0.7],
+#                                             'max_iter':[10000]})
 
 #grid search for ensemble models
+# class AverageWeight(BaseEstimator, RegressorMixin):
+#     def __init__(self, mod, weight):
+#         self.mod = mod
+#         self.weight = weight
+#
+#     def fit(self, X, y):
+#         self.models_ = [clone(x) for x in self.mod]
+#         for model in self.models_:
+#             model.fit(X, y)
+#         return self
+#
+#     def predict(self, X):
+#         w = list()
+#         pred = np.array([model.predict(X) for model in self.models_])
+#         # for every data point, single model prediction times weight, then add them together
+#         for data in range(pred.shape[1]):
+#             single = [pred[model, data] * weight for model, weight in zip(range(pred.shape[0]), self.weight)]
+#             w.append(np.sum(single))
+#         return w
+
+
+#choose these models because they performed well
+lasso = Lasso(alpha=0.0005,max_iter=10000)
+ridge = Ridge(alpha=60)
+svr = SVR(gamma= 0.0004,kernel='rbf',C=13,epsilon=0.009)
+ker = KernelRidge(alpha=0.2 ,kernel='polynomial',degree=3 , coef0=0.8)
+ela = ElasticNet(alpha=0.005,l1_ratio=0.08,max_iter=10000)
+bay = BayesianRidge()
+
+#weights of selected models based on gridsearch score
+w1 = 0.02
+w2 = 0.2
+w3 = 0.25
+w4 = 0.3
+w5 = 0.03
+w6 = 0.2
+
+#average score across 6 models
+# weight_avg = AverageWeight(mod = [lasso,ridge,svr,ker,ela,bay],weight=[w1,w2,w3,w4,w5,w6])
+# rmse_cv(weight_avg,X_scaled,y_log),  rmse_cv(weight_avg,X_scaled,y_log).mean()
+
+
+#average score across 2 best models
+# weight_avg = AverageWeight(mod = [svr,ker],weight=[0.5,0.5])
+# rmse_cv(weight_avg,X_scaled,y_log),  rmse_cv(weight_avg,X_scaled,y_log).mean()
+
+
+class stacking(BaseEstimator, RegressorMixin, TransformerMixin):
+    def __init__(self, mod, meta_model):
+        self.mod = mod
+        self.meta_model = meta_model
+        self.kf = KFold(n_splits=5, random_state=42, shuffle=True)
+
+    def fit(self, X, y):
+        self.saved_model = [list() for i in self.mod]
+        oof_train = np.zeros((X.shape[0], len(self.mod)))
+
+        for i, model in enumerate(self.mod):
+            for train_index, val_index in self.kf.split(X, y):
+                renew_model = clone(model)
+                renew_model.fit(X[train_index], y[train_index])
+                self.saved_model[i].append(renew_model)
+                oof_train[val_index, i] = renew_model.predict(X[val_index])
+
+        self.meta_model.fit(oof_train, y)
+        return self
+
+    def predict(self, X):
+        whole_test = np.column_stack([np.column_stack(model.predict(X) for model in single_model).mean(axis=1)
+                                      for single_model in self.saved_model])
+        return self.meta_model.predict(whole_test)
+
+    def get_oof(self, X, y, test_X):
+        oof = np.zeros((X.shape[0], len(self.mod)))
+        test_single = np.zeros((test_X.shape[0], 5))
+        test_mean = np.zeros((test_X.shape[0], len(self.mod)))
+        for i, model in enumerate(self.mod):
+            for j, (train_index, val_index) in enumerate(self.kf.split(X, y)):
+                clone_model = clone(model)
+                clone_model.fit(X[train_index], y[train_index])
+                oof[val_index, i] = clone_model.predict(X[val_index])
+                test_single[:, j] = clone_model.predict(test_X)
+            test_mean[:, i] = test_single.mean(axis=1)
+        return oof, test_mean
+
+a = Imputer().fit_transform(X_scaled)
+b = Imputer().fit_transform(y_log.values.reshape(-1,1)).ravel()
+
+# stack_model = stacking(mod=[lasso,ridge,svr,ker,ela,bay],meta_model=ker)
+#
+# X_train_stack, X_test_stack = stack_model.get_oof(a,b,test_X_scaled)
+# X_train_add = np.hstack((a,X_train_stack))
+# X_test_add = np.hstack((test_X_scaled,X_test_stack))
+
+stack_model = stacking(mod=[lasso,ridge,svr,ker,ela,bay],meta_model=ker)
+stack_model.fit(a,b)
+pred = np.exp(stack_model.predict(test_X_scaled))
+
+#submission
+
+results = pd.DataFrame({'Id':df_test.Id, 'SalePrice':pred})
+results.to_csv('submission.csv', index=False)
